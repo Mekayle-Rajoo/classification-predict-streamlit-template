7efa341fc834943b0f359a56ed6a8c37e7548624
Mekayle Rajoo <mekayle@gmail.com>
2022-06-27 18:37:30 +0200

    Streamlit App with all models

--- base_app.py
+++ base_app.py
@@ -35,6 +35,40 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
+def clean_text(mystring):
+    
+    from nltk.corpus import stopwords
+    from nltk.tokenize import word_tokenize
+    import string
+    import re
+    from nltk.tokenize import word_tokenize, TreebankWordTokenizer
+    from nltk.stem import WordNetLemmatizer
+    
+    mystring = re.sub('http[s]?://(?:[A-Za-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9A-Fa-f][0-9A-Fa-f]))+', 'url-web', mystring)
+    
+    mystring = mystring.lower()
+    
+    without_stopwords = []
+    for tweet in range(len(mystring)):
+        split = mystring.split(" ")
+        for word in stopwords.words('english'):
+            if word in split:
+                split.remove(word)
+        without_stopwords.append(' '.join(map(str, split)))
+    
+    mystring = without_stopwords[0]
+    
+    mystring = mystring.translate(str.maketrans('', '', string.punctuation))
+    
+    lemmatizer = WordNetLemmatizer()
+    
+    mystring = ' '.join([lemmatizer.lemmatize(w) for w in word_tokenize(mystring)])
+    
+        
+    return mystring  
+
+
+
 # The main function where we will build the actual app
 def main():
 	"""Tweet Classifier App with Streamlit """
@@ -46,15 +80,25 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information", "Meet the team"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
+	if selection == "Meet the team":
+		st.markdown("Our Team:")
+		st.text("Supervisor: Claudia Elliot-Wilson")       
+		st.text("Bethuel Masango")
+		st.text("Kanego Kgabalo Makhuloane")
+		st.text("Madute Ledwaba")
+		st.text("Mekayle Rajoo")
+		st.text("Mosuwe Mosibi")
+		st.text("Thabang Rodney Mabelane")
+    
+    
+    
 	if selection == "Information":
-		st.info("General Information")
+		st.info("Below is some information about the models as well as the data we trained these models on")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
@@ -62,16 +106,35 @@ def main():
 	# Building out the predication page
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
+		model_option = st.selectbox('Please select a Classification Model:', ["Logistic Regression", "Linear Support Vector                 		Classifier","Multinomial Naive Bayes Classifier", "AdaBoostClassifier" ])
+        
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+		tweet_text = st.text_area("Enter Text","Type a Tweet Here")
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
+			tweet_text = clean_text(tweet_text)             
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/LSVC_model.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+			if model_option == "Linear Support Vector                 		Classifier":
+						predictor = joblib.load(open(os.path.join("resources/LSVC_model.pkl"),"rb"))
+						prediction = predictor.predict(vect_text)
+                        
+			if model_option == "Logistic Regression":
+						predictor = joblib.load(open(os.path.join("resources/LG_model.pkl"),"rb"))
+						prediction = predictor.predict(vect_text)
+                        
+			if model_option == "Multinomial Naive Bayes Classifier":
+						predictor = joblib.load(open(os.path.join("resources/MNBC_model.pkl"),"rb"))
+						prediction = predictor.predict(vect_text) 
+                        
+			if model_option == "AdaBoostClassifier":
+						predictor = joblib.load(open(os.path.join("resources/ABC_model.pkl"),"rb"))
+						prediction = predictor.predict(vect_text) 
+                        
+                        
+                        
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
